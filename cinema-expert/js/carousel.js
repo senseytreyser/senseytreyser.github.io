8007687a2842f57c878b241467f30a39de365c3b
Senseytreyser <senseytreyser@mail.ru>
2019-04-27 08:23:53 +0300

    Добавление js кода

---
+++ cinema-expert/js/carousel.js
@@ -0,0 +1,18 @@
+﻿function carouselClick(){
+    var carousel = document.querySelector("#carousel");
+    var largeImg = carousel.querySelector(".carousel__window");
+
+    carousel.onclick = function(event){
+        var target = event.target; 
+        if (target.classList.contains("carousel__img")){
+            changeLargeImage(target, largeImg);
+            return;
+        }
+        return false;
+    }   
+}
+
+function changeLargeImage(target, largeImg){
+    var lageImgLink = target.getAttribute("src");
+    largeImg.setAttribute("src",lageImgLink);
+}
---
+++ cinema-expert/js/tabs.js
@@ -0,0 +1,12 @@
+﻿function tabClick(){
+    var container = document.getElementById("interesting");
+    var btns = contains.querySelectorAll(".interesting__btn");
+    var tabs = contains.querySelectorAll(".interesting__tab");
+
+    container.onclick() = function(e){
+        var target = e.target;
+        if (target.classList.contains(".interesting__btn")){
+            target.classList.toggle("checked");
+        }
+    }
+}
